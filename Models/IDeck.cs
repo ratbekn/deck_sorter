bcfb0a7663a1da864f74f227a61594e84b784bc7
Ратбек Нурланбек уулу <ratbeknurlanbekuulu@gmail.com>
2021-06-13 14:04:27 +0500

    Создать модель игральной колоды

---
+++ Models/IDeck.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DeckSorter.Models
+{
+    public interface IDeck<T> where T : ICard
+    {
+        public string Name { get; }
+        public IEnumerable<T> Cards { get; }
+    }
+}
---
+++ Models/PlayingDeck.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DeckSorter.Models
+{
+    public class PlayingDeck : IDeck<PlayingCard>
+    {
+        public string Name { get; }
+        public IEnumerable<PlayingCard> Cards { get; }
+    }
+}
